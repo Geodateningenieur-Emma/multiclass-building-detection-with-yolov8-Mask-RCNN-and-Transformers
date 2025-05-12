63db641438fe4c611103f7bec85c8c63f8dcabb7
Geodateningenieur-Emma <enyandwi7@gmail.com>
2025-05-12 19:54:05 +0200

    Add files via upload

---
+++ Models/config.py
@@ -0,0 +1,17 @@
+ALL_CLASSES = [
+    '_background_',
+    'high',
+    'low'
+]
+
+LABEL_COLORS_LIST = [
+    [0,0,0],
+    [128,0,0],
+    [0,128,0]
+]
+
+VIS_LABEL_MAP = [
+    [0,0,0],
+    [179, 179, 255],
+    [204, 0, 0]
+]
