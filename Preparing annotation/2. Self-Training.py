9267ef75c19c2e4603d0bf4c9514d4c32cec7a69
enyandwi7 <enyandwi7@gmail.com>
2025-05-14 09:40:56 +0200

    Update 2. Self-Training.py

--- Preparing annotation/2. Self-Training.py
+++ Preparing annotation/2. Self-Training.py
@@ -1,7 +1,7 @@
 
 """
-Self-training process. 
-We use a simple classification. A pre-trained yolo classifier. Install Ultralytics by following the instructions from https://docs.ultralytics.com/
+We use a pre-trained YOLO classifier for self-training and pseudolabel generation. 
+Install Ultralytics by following the instructions from https://docs.ultralytics.com/
 If you have imbalanced classes, consider using the Weighted Loss Function (you may follow through https://github.com/ultralytics/ultralytics/issues/2208). This was the case in our implementation. the high class contained few samples compared to low class.
 The modification is done by locating the YAML file example, C:\Program Files\Python312\Lib\site-packages\ultralytics\utils\loss and the __call__ method was customized to compute class-weighted cross-entropy loss using frequency-defined class weights ([2.4, 1.8]).
 The modification looks as follows:
@@ -12,11 +12,19 @@ class v8ClassificationLoss:
         #loss = F.cross_entropy(preds, batch["cls"], reduction="mean") # old line
         loss_items = loss.detach()
         return loss, loss_items
+
+The structure of the project is as follows:
+trainSetOfFewLabelledRoofCrops
+├── train
+│   ├── 1(low)  [n images]
+│   └── 2 (high)[m images]
+└── valid
+    ├── 1(low)  [s images]
+    └── 1(low)  [t images]
 """
-############################################### 1: TRAIN ON LABELLED FEW SAMPLES  #################################################################
+#1: TRAIN ON LABELLED FEW SAMPLES  #################################################################
 # Import required libraries 
 from ultralytics import YOLO
-
 import os
 os.chdir("workingDir")  
 # Load a model
@@ -28,11 +36,10 @@ model = YOLO('YOLOv8x-cls.yaml').load('add pretrained model path here')  # build
 results = model.train(data='./trainSetOfFewLabelledRoofCrops', epochs=300, imgsz=224) # the default size is 224
 
 metrics = model.val()  # no arguments needed, dataset and settings remembered
-metrics.top1   # top1 accuracy
-metrics.top5   # top5 accuracy
 
-##################################################### 2. LEVERAGE PREDICTION WITH HIGH CONFIDENCE TO EXPDAND THE TRAINING SET ########################
-#make classification and write result to csv file 
+# 2. LEVERAGE PREDICTION WITH HIGH CONFIDENCE TO EXPAND THE TRAINING SET ########################
+
+#make classification and write the result to csv file 
 import os
 import csv
 import numpy as np
@@ -42,10 +49,10 @@ from PIL import Image
 model = YOLO('./bestModel.pt')
 model.to("cpu")
 
-# Define classes names
+# Define class names
 classes_names = ["1", "2"]
 
-# Define folder containing images
+# Define folder containing images for pseudolabelling
 folder_path = "./allsampleRoofCrops"
 
 # Define CSV file path to save results
@@ -73,10 +80,10 @@ with open(csv_file_path, 'w', newline='') as csvfile:
 
                 # Save results to CSV
                 writer.writerow({'image': filename, 'Class': class_name, 'Probability': probability})
-                              
 
-#Create a  new training dataset containing pseudo labels  which confidence is very high 
-import os
+#####################   expand annotationDataSet ##################################################                            
+
+#Create a  new training dataset containing pseudo labels whose confidence is very high 
 import shutil
 import pandas as pd
 
@@ -86,7 +93,7 @@ def create_class_subsets(df, source_dir, target_dir):
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
     
-    # Create 2 subfolders (1 for High,  2 for Low)
+    # Create 2 subfolders (1 for Low,  2 for High)
     for class_name in [1, 2]:
         class_folder = os.path.join(target_dir, str(class_name))
         os.makedirs(class_folder, exist_ok=True)
@@ -107,33 +114,32 @@ def create_class_subsets(df, source_dir, target_dir):
         else:
             print(f"Image {image_name} not found in source directory.")
 
-
-# Example usage
-df = pd.read_csv('C:/dl/yolo/selftraining/sample_class_wealth_sample.csv')
+# Apply the function to unlabelled roof patches to generate pseudolabels
+df = pd.read_csv('./sample_class_wealth_sample.csv')
 df=df[df['Probability']>=0.9] 
-source_directory = 'C:/dl/yolo/selftraining/sat2022'  # Source folder where the images are stored
-target_directory = 'C:/dl/yolo/selftraining/student1sat'  # Target folder where the balanced dataset will be created
+source_directory = './UnlabelledTrainSetRoofPatches'  # Source folder where the images are stored
+target_directory = './TrainSetRoofPatchesPseudolabels'  # Target folder where the balanced dataset will be created
 
 create_class_subsets(df, source_directory, target_directory)
 
 """
-retrain model as in 1 and repeate the process 3-4 times until maximum of samples are classified with high accuracy. The final model will apply to all samples to geenrate the 
+retrain model as in 1 and repeat the process 3-4 times until a maximum of samples are classified with high accuracy. The final model will apply to all samples to geenrate the 
 classified map of roofs
 """
 
-########################### refine the dataframe with roofPatches ID and their assigned class and join this to orginal shapefile#############################
+########################### refine the dataframe with roofPatches ID and their assigned class and join this to the original shapefile#############################
       
 import pandas as pd
 
 # Load the CSV file
-file_path = "C:/dl/MT/Musanze/inferencenf.csv"  # Replace with your actual file path
+file_path = "./sample_class_wealth_sample.csv"  # Replace with your actual file path
 df = pd.read_csv(file_path)
 
 # Remove the .png extension from the "image" column
-df['image'] = df['image'].str.replace('.png', '', regex=False) # remove the extension to remin with image ID to make join to shapefile possible
+df['image'] = df['image'].str.replace('.png', '', regex=False) # remove the extension to remain with image ID to make join to shapefile possible
 
 # Save the modified DataFrame back to a CSV file
-output_path = 'C:/dl/MT/Musanze/modified_inferencenf.csv'  # Replace with your desired output file path
+output_path = './modified_sample_class_wealth_sample.csv'  # Replace with your desired output file path
 df.to_csv(output_path, index=False)
 
 print(f"Modified file saved to {output_path}")
